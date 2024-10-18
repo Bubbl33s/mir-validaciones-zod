437ff3735a3a21351b745dc7611948d55312b7bf
Bubbl33s <jairo.lf.8@gmail.com>
2024-10-18 09:14:19 -0500

    feat: add docs to middleware

--- src/middlewares/validations.ts
+++ src/middlewares/validations.ts
@@ -2,19 +2,31 @@ import { Request, Response, NextFunction } from "express";
 import { Product } from "../validations/product.validations";
 import { z } from "zod";
 
+// El middleware intercepta la solicitud, valida el cuerpo de la solicitud
 export const validateProduct = (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
   try {
+    // Valida el cuerpo de la solicitud en base al esquema de validación de producto
     Product.parse(req.body);
+    // Si la validación es exitosa, pasa al siguiente middleware
     next();
   } catch (error) {
+    // Si la validación falla se maneja el error
     if (error instanceof z.ZodError) {
-      res.status(400).send(error.message);
+      // Si hay errores de validación, formatea los errores y los envía en la respuesta
+      const formattedErrors = error.errors.map((err) => ({
+        path: err.path.join("."),
+        message: err.message,
+      }));
+
+      res.status(400).json({ errors: formattedErrors });
       return;
     }
+
+    // Si hay un error interno del servidor, envía un mensaje de error genérico
     res.status(500).send("Internal Server Error");
   }
 };
